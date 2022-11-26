7551614b887c86730d3d27a6669d3b1012dad4da
Oliveiralucass <gf.oliveiralucas@gmail.com>
2022-11-25 22:33:07 -0300

    Adicionado tela de login

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,14 +1,12 @@
 import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { Login } from './pages/Login/Login'
-import { SignUp } from './pages/SignUp/SignUp'
 
 export const AppRoutes = () => {
   return (
     <BrowserRouter>
         <Routes>
             <Route path='/' element={<Login />} />
-            <Route path='/sign-up' element={<SignUp />}/>
         </Routes>
     </BrowserRouter>
   )
