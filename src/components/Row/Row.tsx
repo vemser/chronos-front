6aa348760dee194671952b6a803c975fbda5c683
Gustavo Scopinho <102439841+GustavoScopinho@users.noreply.github.com>
2022-11-26 17:51:18 -0300

    Arrumando Row

--- src/components/Row/Row.tsx
+++ src/components/Row/Row.tsx
@@ -1,17 +1,81 @@
-import React from 'react'
-import { DataGrid } from '@mui/x-data-grid'
+import * as React from 'react'
+import {
+  TableCell,
+  TableContainer,
+  TableHead,
+  Table,
+  TableBody,
+  TableRow
+} from '@mui/material'
+import ModeEditSharpIcon from '@mui/icons-material/ModeEditSharp'
+import DeleteSharpIcon from '@mui/icons-material/DeleteSharp'
+import styles from './Row.module.css'
 
 export const Row: React.FC = () => {
   return (
-    <div style={{ height: 250, width: '100%' }}>
-      <DataGrid
-        columns={[{ field: 'name' }]}
-        rows={[
-          { internalId: 1, name: 'React' },
-          { internalId: 2, name: 'MUI' }
-        ]}
-        getRowId={row => row.internalId}
-      />
-    </div>
+    <TableContainer
+      sx={{ boxShadow: 2, width: 'auto', mt: 2, borderRadius: '5px' }}
+    >
+      <Table>
+        <TableHead sx={{ backgroundColor: '#1e62fe', borderRadius: '8px' }}>
+          <TableRow>
+            <TableCell sx={{ color: '#fff' }}>Colaborador(a)</TableCell>
+            <TableCell sx={{ color: '#fff' }}>E-mail</TableCell>
+            <TableCell sx={{ color: '#fff' }}>Cargo</TableCell>
+            <TableCell sx={{ color: '#fff' }}>Ações</TableCell>
+          </TableRow>
+        </TableHead>
+
+        <TableBody>
+          {/* {rows.map(row => ( */}
+          <TableRow>
+            <TableCell>Henrique Soares</TableCell>
+            <TableCell>henrique@hotmail.com </TableCell>
+            <TableCell>Gestor de Pessoas</TableCell>
+            <TableCell>
+              <ModeEditSharpIcon
+                sx={{ mr: 1, cursor: 'pointer' }}
+                className={styles.ButtonContainer}
+              />
+              <DeleteSharpIcon
+                sx={{ cursor: 'pointer' }}
+                className={styles.ButtonContainer}
+              />
+            </TableCell>
+          </TableRow>
+
+          <TableRow>
+            <TableCell>Henrique Soares</TableCell>
+            <TableCell>henrique@hotmail.com </TableCell>
+            <TableCell>Gestor de Pessoas</TableCell>
+            <TableCell>
+              <ModeEditSharpIcon sx={{ mr: 1, cursor: 'pointer' }} />
+              <DeleteSharpIcon sx={{ cursor: 'pointer' }} />
+            </TableCell>
+          </TableRow>
+
+          <TableRow>
+            <TableCell>Henrique Soares</TableCell>
+            <TableCell>henrique@hotmail.com </TableCell>
+            <TableCell>Gestor de Pessoas</TableCell>
+            <TableCell>
+              <ModeEditSharpIcon sx={{ mr: 1, cursor: 'pointer' }} />
+              <DeleteSharpIcon sx={{ cursor: 'pointer' }} />
+            </TableCell>
+          </TableRow>
+
+          <TableRow>
+            <TableCell>Henrique Soares</TableCell>
+            <TableCell>henrique@hotmail.com </TableCell>
+            <TableCell>Gestor de Pessoas</TableCell>
+            <TableCell>
+              <ModeEditSharpIcon sx={{ mr: 1, cursor: 'pointer' }} />
+              <DeleteSharpIcon sx={{ cursor: 'pointer' }} />
+            </TableCell>
+          </TableRow>
+          {/* ))} */}
+        </TableBody>
+      </Table>
+    </TableContainer>
   )
 }
--- src/pages/Pagina Inicial/PaginaInicial.tsx
+++ src/pages/Pagina Inicial/PaginaInicial.tsx
@@ -2,12 +2,17 @@ import React from 'react'
 import { EditarPerfil } from '../../components/EditarPerfil/EditarPerfil'
 import { Header } from '../../components/Header/Header'
 import { Row } from '../../components/Row/Row'
+import { Box } from '@mui/system'
 
 export const PaginaInicial: React.FC = () => {
   return (
     <>
       <Header />
-      <Row />
+      <Box width={'100%'} display={'flex'} justifyContent={'center'}>
+        <Box width={'80%'}>
+          <Row />
+        </Box>
+      </Box>
     </>
   )
 }
