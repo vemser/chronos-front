237e8b4919feb76a2b4c6f32056e84e3de81112e
Gustavo Scopinho <102439841+GustavoScopinho@users.noreply.github.com>
2022-11-26 21:38:10 -0300

    Add Cadastrar Colaborador

--- src/pages/Pagina Inicial/PaginaInicial.tsx
+++ src/pages/Pagina Inicial/PaginaInicial.tsx
@@ -3,16 +3,18 @@ import { EditarPerfil } from '../../components/EditarPerfil/EditarPerfil'
 import { Header } from '../../components/Header/Header'
 import { Row } from '../../components/Row/Row'
 import { Box } from '@mui/system'
+import { CadastrarColaborador } from '../CadastrarColaborador/CadastrarColaborador'
 
 export const PaginaInicial: React.FC = () => {
   return (
     <>
       <Header />
-      <Box width={'100%'} display={'flex'} justifyContent={'center'}>
+      <CadastrarColaborador />
+      {/* <Box width={'100%'} display={'flex'} justifyContent={'center'}>
         <Box width={'80%'}>
           <Row />
         </Box>
-      </Box>
+      </Box> */}
     </>
   )
 }
