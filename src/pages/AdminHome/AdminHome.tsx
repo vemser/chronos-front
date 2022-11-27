b64f361bc52f25e17f80bc9ad1233c9ede44b4a2
Oliveiralucass <gf.oliveiralucas@gmail.com>
2022-11-27 02:51:31 -0300

    Adicionado pages das rotas

---
+++ src/pages/AdminHome/AdminHome.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Header } from '../../components/Header/Header'
+import { Row } from '../../components/Row/Row'
+import { Box } from '@mui/system'
+
+export const AdminHome: React.FC = () => {
+  return (
+    <>
+      <Header />
+      <Box width={'100%'} display={'flex'} justifyContent={'center'}>
+        <Box width={'80%'}>
+          <Row />
+        </Box>
+      </Box>
+    </>
+  )
+}
