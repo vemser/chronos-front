c50559d0aeaf7faad6d59a280ec4a8bffc99cacf
Gustavo Scopinho <102439841+GustavoScopinho@users.noreply.github.com>
2022-11-28 12:06:47 -0300

    Add tela Gestão Nova Etapa

--- src/pages/Gestao/GestaoNovaEtapa/GestaoNovaEtapa.tsx
+++ src/pages/Gestao/GestaoNovaEtapa/GestaoNovaEtapa.tsx
@@ -1,8 +1,35 @@
 import React from 'react'
-import styles from 'GestaoNovaEtapa.module.css'
+import styles from './GestaoNovaEtapa.module.css'
+import { GestaoHeader } from '../../../components/Gestao/GestaoHeader/GestaoHeader'
+import { TextField } from '@mui/material'
+import Button from '@mui/material/Button'
 
 export const GestaoNovaEtapa = () => {
   return (
-    <div>GestaoNovaEtapa</div>
+    <>
+      <GestaoHeader />
+      <div className={styles.ContainerGeral}>
+        <div className={styles.ContainerTitle}>
+          <h2>Cadastrar etapa</h2>
+        </div>
+        <div className={styles.ContainerCadastrarEtapa}>
+          <TextField
+            id="standard-multiline-flexible"
+            label="Nome"
+            variant="standard"
+            className={styles.Nome}
+          />
+          <TextField
+            id="standard-multiline-flexible"
+            label="Ordem"
+            variant="standard"
+            className={styles.Nome}
+          />
+          <Button variant="contained" className={styles.Button}>
+            Enviar
+          </Button>
+        </div>
+      </div>
+    </>
   )
 }
