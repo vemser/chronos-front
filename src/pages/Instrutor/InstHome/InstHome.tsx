cb6f80bc1e4c160120d4c8e29562378dfdfffea8
Gustavo Scopinho <102439841+GustavoScopinho@users.noreply.github.com>
2022-12-05 21:27:33 -0300

    Merge

--- src/pages/Instrutor/InstHome/InstHome.tsx
+++ src/pages/Instrutor/InstHome/InstHome.tsx
@@ -3,6 +3,7 @@ import { AuthContext } from '../../../context/AuthContext'
 
 import { Calendario } from '../../../components/Calendario/Calendario'
 import { CalendarioGeral } from '../../../components/CalendarioGeral/CalendarioGeral'
+import { Header } from '../../../components/Header/Header'
 
 export const InstHome = () => {
   const { loggedUser } = useContext<any>(AuthContext)
